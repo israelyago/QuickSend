27bba938ae0e7dad66caa1ba917b8f384ef1eef9
Israel Yago Pereira <26233246+israelyago@users.noreply.github.com>
2024-07-31 13:51:04 -0300

    Allow more files to be added

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,40 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import { open as openFileDialog } from "@tauri-apps/api/dialog";
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
 import "./App.css";
 
 interface GetShareCodeResponse {
-  doc_ticket: string,
+  doc_ticket: string;
 }
 
 function App() {
   const [blob_ticket, setBlobTicket] = useState("");
   const [doc_ticket, setDocTicket] = useState("");
+  const [pathsSelected, setPathsSelected] = useState([""]);
 
   async function get_blob() {
     let toast_id = toast.loading("Downloading ...");
-    invoke<string>("get_blob", { getBlobRequest: { blob_ticket: blob_ticket } })
-    .then(msg => {
-      toast.success(<p>{msg}</p>, {
-        id: toast_id,
-      })
-    }, err => {
-      toast.error(<b>{err}</b>, {
-        id: toast_id,
-      })
-    });
+    invoke<string>("get_blob", {
+      getBlobRequest: { blob_ticket: blob_ticket },
+    }).then(
+      (msg) => {
+        toast.success(<p>{msg}</p>, {
+          id: toast_id,
+        });
+      },
+      (err) => {
+        toast.error(<b>{err}</b>, {
+          id: toast_id,
+        });
+      },
+    );
   }
 
   async function get_share_code(paths: string[]) {
-    let r: GetShareCodeResponse = await invoke("get_share_code", { getShareCodeRequest: { files: paths } });
+    let r: GetShareCodeResponse = await invoke("get_share_code", {
+      getShareCodeRequest: { files: paths },
+    });
     if (r["doc_ticket"]) {
       setDocTicket(r["doc_ticket"]);
     } else {
@@ -39,12 +46,16 @@ function App() {
     openFileDialog({
       title: "Select the files to be sent",
       multiple: true,
-    }).then(async paths => {
-      if (paths === null || paths instanceof String) {
-        return;
-      }
-      await get_share_code(paths as string[]);
-    }).catch(console.error);
+    })
+      .then(async (paths) => {
+        if (paths === null || paths instanceof String) {
+          return;
+        }
+        const all_paths = [...new Set([...pathsSelected, ...paths])];
+        setPathsSelected(all_paths);
+        await get_share_code(all_paths);
+      })
+      .catch(console.error);
   }
 
   return (
@@ -56,15 +67,25 @@ function App() {
         }}
       >
         <h2 className="text-3xl text-center dark:text-gray-300">Send</h2>
-        <button className="w-fit self-center p-3 rounded-2xl border-4 border-slate-700 hover:bg-slate-700 hover:text-slate-50 dark:text-gray-300"
-          onClick={show_file_dialog}>Select files</button>
-      
+        <button
+          className="w-fit self-center p-3 rounded-2xl border-4 border-slate-700 hover:bg-slate-700 hover:text-slate-50 dark:text-gray-300"
+          onClick={show_file_dialog}
+        >
+          {pathsSelected.length == 0 ? "Select files" : "Select more files"}
+        </button>
 
-        {doc_ticket ? <div className="">
-          <h3 className="dark:text-gray-300">Super secret code:</h3>
-          <p className="dark:text-gray-300">{doc_ticket}</p>
-        </div>: null}
+        <div>
+          {pathsSelected.map((p) => {
+            return <div className="dark:text-gray-300">{p}</div>;
+          })}
+        </div>
 
+        {doc_ticket ? (
+          <div className="">
+            <h3 className="dark:text-gray-300">Super secret code:</h3>
+            <p className="dark:text-gray-300">{doc_ticket}</p>
+          </div>
+        ) : null}
       </form>
       <hr className="dark:border-zinc-700" />
 
@@ -74,7 +95,7 @@ function App() {
           e.preventDefault();
           get_blob();
         }}
-        >
+      >
         <h2 className="text-3xl text-center dark:text-gray-300">Receive</h2>
         <input
           className="p-3 rounded-2xl border-4 border-slate-700"
@@ -84,20 +105,23 @@ function App() {
         />
         <button
           className="p-3 rounded-2xl border-4 border-slate-700 hover:bg-slate-700 hover:text-slate-50 dark:text-gray-300"
-          type="submit">Download</button>
+          type="submit"
+        >
+          Download
+        </button>
       </form>
 
       <Toaster
         position="bottom-center"
         toastOptions={{
-          className: '',
+          className: "",
           duration: 5000,
           style: {
-            background: '#363636',
-            color: '#fff',
+            background: "#363636",
+            color: "#fff",
           },
         }}
-        />
+      />
     </div>
   );
 }
